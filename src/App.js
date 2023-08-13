0f9cb7d60e40beac4e53ff1dc8edab9342764fe8
kiatklai <likhitthaworn.k@gmail.com>
2023-08-13 13:56:58 +0900

    検索できるように

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 function App() {
   const [countries,setCounties] = useState([])
+  const [word,setWord] = useState("")
+  const [dataFilter] = useState(["name","capital"])
 
   useEffect(()=>{
     fetch("https://restcountries.com/v2/all")
@@ -12,10 +14,28 @@ function App() {
     })
   },[])
 
+  const searchCountries=(countries)=>{
+    return countries.filter((item)=>{
+      return dataFilter.some((filter)=>{
+        return item[filter].toString().toLowerCase().indexOf(word.toLowerCase())>-1
+      })
+    })
+  }
+
   return (
     <div className='container'>
+      <div className='search-container'>
+        <label htmlFor='search-form'>
+          <input type='text' 
+          className='search-input' 
+          placeholder='search any country'
+          value={word}
+          onChange={(e)=>setWord(e.target.value)}
+          />
+        </label>
+      </div>
      <ul className='row'>
-      {countries.map((item,index)=>{
+      {searchCountries(countries).map((item,index)=>{
         return (  
             <li key={index}>
               <div className='card'>
