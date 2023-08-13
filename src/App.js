55fe72d289cf998dd6caf6e85d41943652216296
kiatklai <likhitthaworn.k@gmail.com>
2023-08-13 13:04:52 +0900

    API取得/CSSデザイン変更

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 import './App.css';
 
 function App() {
-  const data = [
-    {id:1,name:"Thailand",region:"Asia",population:100},
-    {id:2,name:"Japan",region:"Asia",population:100}
-  ]
+  const [countries,setCounties] = useState([])
 
-  const [countries,setCounties] = useState(data)
+  useEffect(()=>{
+    fetch("https://restcountries.com/v2/all")
+    .then(res=>res.json())
+    .then(data=>{
+      setCounties(data)
+    })
+  },[])
 
   return (
     <div className='container'>
-     <ul>
-      {countries.map((item)=>{
-        return <li key={item.id}>{item.name} | {item.region}</li>
+     <ul className='row'>
+      {countries.map((item,index)=>{
+        return (  
+            <li key={index}>
+              <div className='card'>
+                <div className='card-title'>
+                  <img src={item.flag} alt={item.name}/>
+                </div>
+                <div className='card-body'>
+                  <div className='card-description'>
+                    <h2>{item.name}</h2>
+                    <ol>
+                      <li>人口：<span>{item.population}</span></li>
+                      <li>エリア：<span>{item.region}</span></li>
+                      <li>首都：<span>{item.capital}</span></li>
+                    </ol>
+                  </div>
+                </div>
+              </div>
+            </li>
+        )
       })}
      </ul>
     </div>
